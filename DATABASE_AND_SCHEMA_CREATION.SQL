ab7eec385b3aaa4067152e5008b313f0ba931a57
Rasika_Venkadesh <113516327+Rasika-Venkadesh@users.noreply.github.com>
2024-05-29 18:45:36 +0530

    Create DATABASE and SCHEMA

---
+++ DATABASE_AND_SCHEMA_CREATION.SQL
@@ -0,0 +1,7 @@
+-- Database creation
+create database order_db;
+
+-- Schema creation
+create schema ORDER__BASE;  -- For Table creation
+create schema ORDER__XFRM;  -- For Transformation logic
+create schema ORDER__ANALYSIS;  -- For End user's final view
